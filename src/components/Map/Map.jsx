d647cb95274e33d243f1a309e74a31dafa7f5962
Huikang Huang <huikanghuang1@gmail.com>
2022-05-27 19:39:11 -0700

    added basic navbar and map section

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,30 @@
 import React from "react"
+import GoogleMapReact from "google-map-react"
+import { Paper, Typograph, useMediaQuery } from "@material-ui/core"
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
+import Rating from "@material-ui/lab"
+import useStyles from "./styles"
 
 export default function Map() {
+
+    const classes = useStyles()
+    const isMobile = useMediaQuery('(min-width:600px)')
+    const coordinates = {lat: 0, lng: 0}
+
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+            bootstrapURLKeys={{ key:'AIzaSyAoUwp7x4vX0mR7t-qTAq2-eueMrHQlD9s' }}
+            defaultCenter={coordinates}
+            center={coordinates}
+            defaultZoom={14}
+            margin={[50, 50, 50, 50]}
+            options={''}
+            onChange={''}
+            onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     )
 }
